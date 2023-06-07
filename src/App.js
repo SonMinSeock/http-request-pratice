823ca84a4d345946ee074d89040243edb0c54c06
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-06-07 12:20:29 +0900

    Starwars Movie GET Request code transform  async-await

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMovieHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformMovies = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(transformMovies);
-      })
-      .catch((err) => console.log(err));
+  async function fetchMovieHandler() {
+    try {
+      const res = await fetch("https://swapi.dev/api/films");
+      const data = await res.json();
+      const transformMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformMovies);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
